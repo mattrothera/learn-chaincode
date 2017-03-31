f7f9a2fe9be7034374089a224143105e07d8e5c4
Matt Rothera <mattrothera@MattsIBM-MacBook-Pro-2.local>
2017-03-30 19:27:18 -0500

    Compiled my code

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+# See what files have changed locally.  You should see chaincode_start.go
+git status
+# Stage all changes in the local repository for commit
+git add --all
+# Commit all staged changes.  Insert a short description after the -m argument
+git commit -m "Compiled my code"
+# Push local commits back to https://github.com/<YOUR_GITHUB_ID_HERE>/learn-chaincode/
+git push
